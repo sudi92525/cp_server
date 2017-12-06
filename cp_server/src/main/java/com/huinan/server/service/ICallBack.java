8ddcc993182bcaa91f9dd0d4534e803e0ded0ca5
renchao <466038223@qq.com>
2017-12-06 15:17:12 +0800

    del all file

--- cp_server/src/main/java/com/huinan/server/service/ICallBack.java
+++
@@ -1,8 +0,0 @@
-package com.huinan.server.service;
-
-import java.util.Map;
-
-public interface ICallBack<T> {
-
-	void callBack(Map<String, Object> params, T t);
-}
--- proto/proto.bat
+++
@@ -1,4 +0,0 @@
-protoc cp_msg.proto --java_out=.
-protoc cp_msg_cs.proto --java_out=.
-protoc cp_msg_club.proto --java_out=.
-pause;
